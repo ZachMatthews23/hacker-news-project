80f80aeadb90d14eb97a1aa45500e2a6de9c3364
Zachary Matthews <zacharyyog12@gmail.com>
2025-09-29 21:24:30 -0400

    Update prod api url

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hacker-news-project-api-ashcdcddbpbbdzb6.westcentralus-01.azurewebsites.net/api'
+  apiUrl: '/api' // Use Static Web Apps API proxy after linking
 };
