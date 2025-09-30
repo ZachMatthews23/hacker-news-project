a5b3e309de3a2ec4c2094dd8fc93063580cc9ddd
Zachary Matthews <zacharyyog12@gmail.com>
2025-09-30 09:31:25 -0400

    Adjust JSON serialization in api

--- api/Models/HackerNewsItem.cs
+++ api/Models/HackerNewsItem.cs
@@ -1,13 +1,30 @@
+using System.Text.Json.Serialization;
+
 namespace HackerNews.Functions.Models;
 
 public class HackerNewsItem
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("title")]
     public string? Title { get; set; }
+
+    [JsonPropertyName("url")]
     public string? Url { get; set; }
+
+    [JsonPropertyName("by")]
     public string? By { get; set; }
+
+    [JsonPropertyName("time")]
     public long Time { get; set; }
+
+    [JsonPropertyName("score")]
     public int Score { get; set; }
+
+    [JsonPropertyName("descendants")]
     public int Descendants { get; set; }
+
+    [JsonPropertyName("type")]
     public string Type { get; set; } = string.Empty;
 }
--- api/Models/StoriesResponse.cs
+++ api/Models/StoriesResponse.cs
@@ -1,10 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace HackerNews.Functions.Models;
 
 public class StoriesResponse
 {
+    [JsonPropertyName("stories")]
     public List<HackerNewsItem> Stories { get; set; } = new();
+
+    [JsonPropertyName("totalCount")]
     public int TotalCount { get; set; }
+
+    [JsonPropertyName("page")]
     public int Page { get; set; }
+
+    [JsonPropertyName("pageSize")]
     public int PageSize { get; set; }
+
+    [JsonPropertyName("totalPages")]
     public int TotalPages { get; set; }
 }
